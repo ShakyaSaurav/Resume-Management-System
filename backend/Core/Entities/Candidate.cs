86c1edcbc0e46abe7ce398b62dd39f2c996659bc
ShakyaSaurav <sauravshakya2015@gmail.com>
2023-05-23 19:30:07 +0545

    v4 init

---
+++ backend/Core/Entities/Candidate.cs
@@ -0,0 +1,17 @@
+﻿namespace backend.Core.Entities
+{
+    public class Candidate : BaseEntity 
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string CoverLetter { get; set; }
+        public string ResumeURL { get; set; }
+        
+        //relations
+
+        public long JobID { get; set; }
+        public Job Job { get; set; }
+    }
+}
